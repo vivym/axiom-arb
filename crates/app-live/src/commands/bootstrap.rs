457aadf9f4e6948cd3b5926d67505c1d5a8601e0
Ming Yang <ymviv@qq.com>
2026-04-02 01:29:38 +0800

    fix: surface bootstrap command error

--- crates/app-live/src/commands/bootstrap.rs
+++ crates/app-live/src/commands/bootstrap.rs
@@ -1,30 +1,8 @@
-use std::{error::Error, fmt};
+use std::{error::Error, io};
 
 use crate::cli::BootstrapArgs;
 
-#[derive(Debug)]
-struct BootstrapError {
-    message: String,
-}
-
-impl BootstrapError {
-    fn new(message: impl Into<String>) -> Self {
-        Self {
-            message: message.into(),
-        }
-    }
-}
-
-impl fmt::Display for BootstrapError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(&self.message)
-    }
-}
-
-impl Error for BootstrapError {}
-
 pub fn execute(_args: BootstrapArgs) -> Result<(), Box<dyn Error>> {
-    Err(Box::new(BootstrapError::new(
-        "bootstrap is not implemented yet",
-    )))
+    eprintln!("bootstrap is not implemented yet");
+    Err(Box::new(io::Error::other("bootstrap is not implemented yet")))
 }
